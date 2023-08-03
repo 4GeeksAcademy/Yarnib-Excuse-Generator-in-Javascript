46bdc3201bb736f6a3bbad317cccd5876891f23e
Yarnib <138246961+Yarnib@users.noreply.github.com>
2023-08-03 00:02:00 +0000

    Code Refactor

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "chewed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomIndex = array => Math.floor(Math.random() * array.length);
+  const createRandomExcuse = () => {
+    const randomWho = who[getRandomIndex(who)];
+    const randomAction = action[getRandomIndex(action)];
+    const randomWhat = what[getRandomIndex(what)];
+    const randomWhen = when[getRandomIndex(when)];
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  };
+
+  const excuseContainer = document.querySelector("#excuse");
+  excuseContainer.innerHTML = createRandomExcuse();
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
